d696ef786bd037a870250bcbe8fd1a6dde424c56
Edilson <edilsongudo@gmail.com>
2022-07-06 20:54:49 +0200

    Remove venv folder

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kevin/python projects/Web/django_passwords_manager/venv/bin/python3.9
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
