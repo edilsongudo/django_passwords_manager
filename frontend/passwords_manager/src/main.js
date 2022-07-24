0b845211fc938e90b86d5ca9934c9669382b235d
Edilson <edilsongudo@gmail.com>
2022-07-24 12:25:19 +0200

    Adding Vuex

--- frontend/passwords_manager/src/main.js
+++ frontend/passwords_manager/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
 
 import './assets/main.css'
@@ -7,5 +8,6 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
